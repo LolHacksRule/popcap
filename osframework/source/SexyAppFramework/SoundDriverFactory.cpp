98c3fb7f2c05746d3bd6a3487f10f5f380b7a589
Luo Jinghua <sunmoon1997@gmail.com>
2008-11-01 12:28:08 +0800

    SexyAppFramework: add SoundDriverFactory.
    Copy-n-paste it from VideoDriverFactory.

---
+++ osframework/source/SexyAppFramework/SoundDriverFactory.cpp
@@ -0,0 +1,49 @@
+#include "SoundDriverFactory.h"
+
+using namespace Sexy;
+
+SoundDriver::SoundDriver (const std::string theName,
+			  int               thePriority)
+	: Driver(theName, thePriority)
+{
+}
+
+SoundDriver::~SoundDriver ()
+{
+}
+
+SoundDriverFactory::SoundDriverFactory ()
+	: DriverFactory ()
+{
+}
+
+SoundDriverFactory::~SoundDriverFactory ()
+{
+}
+
+SoundDriverFactory*  SoundDriverFactory::GetSoundDriverFactory ()
+{
+	static SoundDriverFactory  * theSoundDriverFactory;
+
+	if (!theSoundDriverFactory)
+		theSoundDriverFactory = new SoundDriverFactory ();
+	return theSoundDriverFactory;
+}
+
+/* This is a hack that preventing gcc from striping drivers out of
+ * binary.
+ */
+extern SoundDriver* GetDummySoundDriver();
+typedef SoundDriver* (* SoundDriverGetter)();
+SoundDriverGetter SoundDriverGetters []= {
+	GetDummySoundDriver,
+	NULL
+};
+
+void SoundDriverFactory::Load(void)
+{
+	int i = 0;
+	for (i = 0; SoundDriverGetters[i]; i++)
+		SoundDriverGetters[i]();
+}
+
---
+++ osframework/source/SexyAppFramework/SoundDriverFactory.h
@@ -0,0 +1,61 @@
+#ifndef __SOUNDDriverFACTORY_H__
+#define __SOUNDDriverFACTORY_H__
+
+#include "Common.h"
+#include "DriverFactory.h"
+#include "SoundManager.h"
+
+namespace Sexy {
+class SoundDriver: public Driver
+{
+ public:
+	virtual SoundManager* Create (SexyAppBase * theApp) = 0;
+
+
+ public:
+	SoundDriver (const std::string theName,
+		     int               thePriority = 0);
+	~SoundDriver ();
+};
+
+
+class SoundDriverFactory: public DriverFactory
+{
+ public:
+	static SoundDriverFactory*  GetSoundDriverFactory ();
+
+ private:
+	void                 Load();
+
+ private:
+	SoundDriverFactory ();
+	~SoundDriverFactory ();
+};
+
+class SoundDriverRegistor
+{
+ public:
+	SoundDriverRegistor(SoundDriver * theDriver)
+		: mDriver (theDriver)
+	{
+		SoundDriverFactory* factory;
+
+		factory = SoundDriverFactory::GetSoundDriverFactory ();
+		factory->AddDriver (mDriver);
+	}
+
+	~SoundDriverRegistor()
+	{
+		SoundDriverFactory* factory;
+
+		factory = SoundDriverFactory::GetSoundDriverFactory ();
+		factory->RemoveDriver (mDriver);
+	}
+
+ private:
+	SoundDriver * mDriver;
+};
+
+}
+
+#endif
