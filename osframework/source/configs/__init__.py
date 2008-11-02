80e25d17c800010f1ed8cdfeef32bae679165bae
Luo Jinghua <sunmoon1997@gmail.com>
2008-11-02 16:18:57 +0800

    scons: add debug/release options.

--- osframework/source/configs/__init__.py
+++ osframework/source/configs/__init__.py
@@ -3,7 +3,11 @@
 # Author: Luo Jinghua
 
 def AddOptions(opts):
-    pass
+    from SCons.Variables.BoolVariable import BoolVariable
+    if 'debug' in opts.keys():
+        return
+    opts.Add(BoolVariable('debug', 'build debug version', 'no'))
+    opts.Add(BoolVariable('release', 'build release version', 'no'))
 
 def Configure(env):
     env.AppendUnique(DRIVERS = [])
--- osframework/source/configs/win32/__init__.py
+++ osframework/source/configs/win32/__init__.py
@@ -3,14 +3,25 @@
 # Author: Luo Jinghua
 
 import configs
-import SCons
 
 def AddOptions(opts):
-    configs.AddOptions(opts)
+    configs.AddOptions (opts)
 
 def Configure(env):
-    configs.Configure(env)
-    env.AppendUnique(CPPDEFINES = ['WIN32'])
-    env.AppendUnique(CFLAGS = ['/EHsc', '/DEBUG'],
-                     CXXFLAGS = ['/EHsc', '/DEBUG'],
-                     LINKFLAGS = [])
+    configs.Configure (env)
+    env.AppendUnique (CPPDEFINES = ['WIN32'])
+    env.AppendUnique (CFLAGS = ['/EHsc'],
+                      CXXFLAGS = ['/EHsc'],
+                      LINKFLAGS = [])
+
+    if env['debug']:
+        env.AppendUnique (CFLAGS = ['/DEBUG'],
+                          CXXFLAGS = ['/DEBUG'],
+                          LINKFLAGS = ['/DEBUG', '/PROFILE',
+                                      '/PDB:${TARGET}.pdb'])
+        env.Replace (CCPDBFLAGS = '/Zi /Fd${TARGET}.pdb')
+
+    if env['release']:
+        env.AppendUnique (CFLAGS = ['/O2'],
+                          CXXFLAGS = ['/O2'],
+                          LINKFLAGS = [])
