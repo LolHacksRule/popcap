c118072051b805c961899540a14cb1df9583199c
Luo Jinghua <sunmoon1997@gmail.com>
2008-11-04 18:58:23 +0800

    scons: add configurate options for linux input driver.

--- osframework/source/configs/linux/__init__.py
+++ osframework/source/configs/linux/__init__.py
@@ -4,14 +4,27 @@
 
 import configs
 
-def AddOptions(opts):
-    configs.AddOptions(opts)
+def AddOptions (opts):
+    configs.AddOptions (opts)
 
-def Configure(env):
-    configs.Configure(env)
-    env.AppendUnique(CFLAGS = ['-pthread'],
-                     LINKFLAGS = ['-pthread'])
-    env.AppendUnique(CFLAGS = ['-g', '-fno-unit-at-a-time', '-Wall'],
-                     CXXFLAGS = ['-g', '-Wall'],
-                     LINKFLAGS = ['-g', '-fno-unit-at-a-time'],
-                     LIBS = ['rt', 'm'])
+def Configure (env):
+    configs.Configure (env)
+    env.AppendUnique (CFLAGS = ['-pthread'],
+                      LINKFLAGS = ['-pthread'])
+    env.AppendUnique (CFLAGS = ['-g', '-fno-unit-at-a-time', '-Wall'],
+                      CXXFLAGS = ['-g', '-Wall'],
+                      LINKFLAGS = ['-g', '-fno-unit-at-a-time'],
+                      LIBS = ['rt', 'm'])
+
+def LunuxInputAddOptions (opts):
+    pass
+
+def EnableLinuxInput (env):
+    pass
+
+def LinuxInputConfigure(env):
+    configs.linux.Configure (env)
+    env.AppendUnique (DRIVERS = ['LINUXINPUT'])
+    linux_input = {}
+    linux_input['ENABLE'] = EnableLinuxInput
+    env.AppendUnique (LINUXINPUT = linux_input)
