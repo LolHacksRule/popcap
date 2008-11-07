4563b8419b251f3b7768c9e2e0845ec4dc1a182d
Luo Jinghua <sunmoon1997@gmail.com>
2008-11-07 14:28:55 +0800

    SexyAppFramework: move event definition to its own header.

--- osframework/source/SexyAppFramework/NativeDisplay.h
+++ osframework/source/SexyAppFramework/NativeDisplay.h
@@ -3,6 +3,7 @@
 #include "Common.h"
 #include "Rect.h"
 #include "CritSect.h"
+#include "Event.h"
 
 namespace Sexy
 {
@@ -13,37 +14,6 @@ class Font;
 class Image;
 class SexyAppBase;
 class Graphics;
-
-enum EventType {
-        EVENT_NONE                  = 0,
-        EVENT_KEY_DOWN              = 1,
-        EVENT_KEY_UP                = 2,
-        EVENT_MOUSE_BUTTON_PRESS    = 3,
-        EVENT_MOUSE_BUTTON_RELEASE  = 4,
-        EVENT_MOUSE_WHEEL_UP        = 5,
-        EVENT_MOUSE_WHELL_DOWN      = 6,
-        EVENT_MOUSE_MOTION          = 7,
-        EVENT_ACTIVE                = 8,
-        EVENT_EXPOSE                = 9,
-        EVENT_QUIT
-};
-
-#define EVENT_FLAGS_AXIS     (1U << 0)
-#define EVENT_FLAGS_REL_AXIS (1U << 1)
-#define EVENT_FLAGS_BUTTON   (1U << 2)
-#define EVENT_FLAGS_KEY_CODE (1U << 3)
-#define EVENT_FLAGS_KEY_CHAR (1U << 4)
-
-struct Event {
-        enum EventType type;
-        unsigned int   flags;
-        int            keyCode;
-        int            keyChar;
-        int            x;
-        int            y;
-        int            button;
-        bool           active;
-};
 
 class NativeDisplay
 {
