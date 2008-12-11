141584f963d55cad617b78a026d556624859d0b6
Luo Jinghua <sunmoon1997@gmail.com>
2008-12-11 15:26:30 +0800

    configs: add configuration for st linux.

---
+++ osframework/source/configs/linux/st/__init__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Author: KanKer
+
+# Spec file for Intel Olo board.
+
+import os.path
+import configs.linux
+
+def AddOptions (opts):
+    configs.linux.AddOptions (opts)
+
+def Configure (env):
+    configs.linux.Configure (env)
+    prefix = '/opt/STM/STLinux-2.2/devkit/sh4'
+    tcdir = os.path.join (prefix, 'bin')
+    targetdir = os.path.join (prefix, 'target')
+    env['prefix'] = prefix
+    env['PKG_CONFIG_LIBDIR'] = os.path.join (prefix, 'lib', 'pkgconfig')
+    env['PKG_CONFIG_LIBDIR'] += ":" + os.path.join (targetdir,
+                                                    'usr', 'lib', 'pkgconfig')
+    env['CC'] = os.path.join (tcdir, 'sh4-linux-gcc')
+    env['CXX'] = os.path.join (tcdir, 'sh4-linux-g++')
+    env['LINK'] = os.path.join (tcdir, 'sh4-linux-g++')
+    env['AR'] = os.path.join (tcdir, 'sh4-linux-ar')
+    env['STRIP'] = os.path.join (tcdir, 'sh4-linux-strip')
+    env['RANLIB'] = os.path.join (tcdir, 'sh4-linux-ranlib')
+    env['LD'] = os.path.join (tcdir, 'sh4-linux-ld')
+    env.AppendUnique (CPPDEFINES = ['SEXY_ST_SH4'])
+    env.AppendUnique (CPPPATH = [os.path.join (prefix, 'include'),
+                                 os.path.join (targetdir, 'include'),
+                                 os.path.join (targetdir, 'usr', 'include')],
+                      LIBPATH = [os.path.join (prefix, 'lib'),
+                                 os.path.join (targetdir, 'lib'),
+                                 os.path.join (targetdir, 'usr', 'lib')],
+                      LIBS = [])
+    env.AppendUnique (LINKFLAGS = ['-export-dynamic'])
+
+    env.Replace (PKGCONFIG = 'PKG_CONFIG_PATH= PKG_CONFIG_LIBDIR=$PKG_CONFIG_LIBDIR pkg-config')
---
+++ osframework/source/configs/linux/st/directfb.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# -*- python -*-
+# Author: Luo Jinghua
+
+import os.path
+import configs.linux.st
+
+def AddOptions (opts):
+    configs.linux.st.AddOptions (opts)
+
+def EnableDirecFB (env):
+    env.AppendUnique (LIBS = ['directfb', 'z', 'fusion', 'direct', 'pthread', 'dl'],
+                      CPPPATH = [os.path.join (env['prefix'], 'target', 'usr',
+                                               'include', 'directfb')],
+                      LIBPATH = [os.path.join (env['prefix'], 'target',
+                                               'usr', 'lib')])
+
+def Configure (env):
+    configs.linux.st.Configure (env)
+    env.AppendUnique (DRIVERS = ['DIRECTFB'])
+    directfb = {}
+    directfb['ENABLE'] = EnableDirecFB
+    env.AppendUnique (DIRECTFB = directfb)
