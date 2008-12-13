11a0b3e637b01dd76e7908cf8b391a787ee39061
Luo Jinghua <sunmoon1997@gmail.com>
2008-12-13 16:49:14 +0800

    scons: add a tool named 'untar' to untaring tarballs.

---
+++ osframework/source/site_scons/site_tools/untar.py
@@ -0,0 +1,66 @@
+import SCons.Action
+import SCons.Builder
+import SCons.Defaults
+import SCons.Node.FS
+import SCons.Util
+import os
+
+def UntarPrefix(target):
+    targets = [ t.path for t in target ]
+    prefix = os.path.commonprefix(targets)
+    return os.path.split(os.path.normpath(prefix))[0]
+
+def Untar(target, source, env):
+    import tarfile
+
+    tar = tarfile.open(str(source[0]), "r")
+
+    prefix = UntarPrefix(target)
+    tar.extractall(prefix)
+    tar.close()
+
+def UntarStr(target, source, env):
+    prefix = UntarPrefix(target)
+    print("Untarring: %s to %s" % (source[0].path, prefix))
+
+def UntarEmitter(target, source, env):
+    import tarfile
+
+    tar = tarfile.open(str(source[0]), "r")
+
+    target = []
+    for member in tar.getmembers():
+        if member.type == tarfile.DIRTYPE:
+            continue
+        entry = env.fs.Entry(member.name)
+        target = target + [entry]
+
+    tar.close()
+
+    ## for t in target:
+    ##     print ("Target(" + repr(t) + "): "+ t.path)
+
+    ## for s in source:
+    ##     print ("Source(" + repr(s) + "): "+ s.path)
+
+    return (target, source)
+
+UntarAction = SCons.Action.Action(Untar, UntarStr)
+UntarBuilder = SCons.Builder.Builder (action = UntarAction,
+                                      emitter = UntarEmitter,
+                                      target_factory = SCons.Node.FS.default_fs.Entry)
+def generate(env):
+    """Add Builders and construction variables for tar to an Environment."""
+
+    try:
+        bld = env['BUILDERS']['Untar']
+    except KeyError:
+        bld = UntarBuilder
+        env['BUILDERS']['Untar'] = bld
+
+def exists(env):
+    try:
+        import tarfile
+        return True
+    except:
+        return False
