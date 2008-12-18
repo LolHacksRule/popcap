c11e2fead694633c74bfa4bcbf6d3123f622ebe5
Luo Jinghua <sunmoon1997@gmail.com>
2008-12-18 22:19:42 +0800

    scons: enhance Untar to be able to filter out files which we don't want to extract.

--- osframework/source/site_scons/site_tools/untar.py
+++ osframework/source/site_scons/site_tools/untar.py
@@ -10,18 +10,35 @@ def UntarPrefix(target):
     prefix = os.path.commonprefix(targets)
     return os.path.split(os.path.normpath(prefix))[0]
 
+def UntarMatch(name, filter_list):
+    for i in filter_list:
+        ### XXX more powerful matcher?
+        ### such as fnmacth or regular expression?
+        if i in name:
+            return True
+    return False
+
 def Untar(target, source, env):
     import tarfile
 
     tar = tarfile.open(str(source[0]), "r")
+    if len (source) > 1:
+        include = source[1].value
+    else:
+        include = []
+    if len (source) > 2:
+        exclude = source[2].value
+    else:
+        exclude = []
 
     prefix = UntarPrefix(target)
     try:
-        try:
-            tar.extractall(prefix)
-        except AttributeError, e:
-            for member in tar.getmembers():
-                tar.extract(member, prefix)
+        for member in tar.getmembers():
+            if len(include) and not UntarMatch (member.name, include):
+                continue
+            if len(exclude) and UntarMatch (member.name, exclude):
+                continue
+            tar.extract(member, prefix)
     finally:
         tar.close()
 
@@ -35,11 +52,23 @@ def UntarEmitter(target, source, env):
     import tarfile
 
     tar = tarfile.open(str(source[0]), "r")
+    if len (source) > 1:
+        include = source[1].value
+    else:
+        include = []
+    if len (source) > 2:
+        exclude = source[2].value
+    else:
+        exclude = []
 
     target = []
     try:
         for member in tar.getmembers():
             if member.type == tarfile.DIRTYPE:
+                continue
+            if len(include) and not UntarMatch (member.name, include):
+                continue
+            if len(exclude) and UntarMatch (member.name, exclude):
                 continue
             entry = env.fs.Entry(member.name)
             target = target + [entry]
@@ -62,10 +91,24 @@ def generate(env):
     """Add Builders and construction variables for tar to an Environment."""
 
     try:
-        bld = env['BUILDERS']['Untar']
+        bld = env['BUILDERS']['_Untar']
     except KeyError:
         bld = UntarBuilder
-        env['BUILDERS']['Untar'] = bld
+        env['BUILDERS']['_Untar'] = bld
+
+    def Untar(source, include = [], exclude = []):
+        if SCons.Util.is_List (include):
+            include = SCons.Node.Python.Value (include)
+        else:
+            include = SCons.Node.Python.Value ([include])
+        if SCons.Util.is_List (exclude):
+            exclude = SCons.Node.Python.Value (exclude)
+        else:
+            exclude = SCons.Node.Python.Value ([exclude])
+        source = [ source, include, exclude ]
+        return env._Untar([], source)
+
+    env.Untar = Untar
 
 def exists(env):
     try:
