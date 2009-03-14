0a42fb319e74d325fc53a88e3e2574b0c5f8e302
Luo Jinghua <sunmoon1997@gmail.com>
2009-03-14 22:35:19 +0800

    SexyAppFramework: make audiere device parameter be configurable via enviroment variable SEXY_AUDIERE_DEVICE.

--- osframework/source/SexyAppFramework/AudiereLoader.cpp
+++ osframework/source/SexyAppFramework/AudiereLoader.cpp
@@ -1,20 +1,25 @@
 /* Original AudiereLoader by Rheenen 2005 */
-
 #include "AudiereLoader.h"
 #include "audiere.h"
 
 audiere::AudioDevicePtr gAudiereDevice;
 audiere::MIDIDevicePtr gAudiereMIDIDevice;
 
-audiere::AudioDevicePtr getAudiereDevice(void) {
-  if (!gAudiereDevice)
-		gAudiereDevice = audiere::OpenDevice(NULL);
+audiere::AudioDevicePtr getAudiereDevice(void)
+{
+	const char * audiereDevice = getenv("SEXY_AUDIERE_DEVICE");
+#ifdef WIN32
+	if (!audiereDevice)
+		audiereDevice = "winmm";
+#endif
+	if (!gAudiereDevice)
+		gAudiereDevice = audiere::OpenDevice(audiereDevice);
 	return gAudiereDevice;
 }
 
-audiere::MIDIDevicePtr getAudiereMIDIDevice(void) {
-  if (!gAudiereMIDIDevice)
+audiere::MIDIDevicePtr getAudiereMIDIDevice(void)
+{
+	if (!gAudiereMIDIDevice)
 		gAudiereMIDIDevice = audiere::OpenMIDIDevice(NULL);
 	return gAudiereMIDIDevice;
 }
-
