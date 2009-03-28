1593a5d892d7855d0e10632ffd6d08ffea5f34ed
Luo Jinghua <sunmoon1997@gmail.com>
2009-03-28 12:57:48 +0800

    SexyAppFramework: hook the Win32 registry implemenation.

--- osframework/source/SexyAppFramework/RegistryInterfaceFactory.cpp
+++ osframework/source/SexyAppFramework/RegistryInterfaceFactory.cpp
@@ -1,9 +1,17 @@
 #include "RegistryInterfaceFactory.h"
 #include "XMLRegistryInterface.h"
 
+#ifdef WIN32
+#include "Win32RegistryInterface.h"
+#endif
+
 using namespace Sexy;
 
 RegistryInterface* RegistryInterfaceFactory::GetInterface(SexyAppBase * theApp)
 {
+#ifdef WIN32
+    return new Win32RegistryInterface(theApp);
+#else
     return new XMLRegistryInterface(theApp);
+#endif
 }
