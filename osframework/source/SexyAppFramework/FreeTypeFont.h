622d3e8fd4587440e5f17a911c0f57b2616c05f2
Luo Jinghua <sunmoon1997@gmail.com>
2009-04-05 14:40:22 +0800

    SexyAppFramework: FreeTypeFont become a wrapper of FreeTypeScaledFont now.

--- osframework/source/SexyAppFramework/FreeTypeFont.h
+++ osframework/source/SexyAppFramework/FreeTypeFont.h
@@ -2,70 +2,13 @@
 #define __FREETYPE_FONT_H__
 
 #include "Font.h"
-#include "FreeTypeBaseFont.h"
+#include "FreeTypeScaledFont.h"
 
 namespace Sexy
 {
 	class Image;
 	class ImageFont;
 	class SexyAppBase;
-	struct FreeTypeGlyphArea;
-	struct FreeTypeGlyphEntry;
-
-	struct  FreeTypeExtents {
-		float x_bearing;
-		float y_bearing;
-		float width;
-		float height;
-		float x_advance;
-		float y_advance;
-	};
-
-	struct FreeTypeGlyph
-	{
-		float x;
-		float y;
-		FT_UInt index;
-
-		FreeTypeGlyphEntry* entry;
-	};
-
-	struct FreeTypeGlyphEntry
-	{
-		// Glyph Image & its rectangle in the image
-		Image* mImage;
-		int    mXOffSet;
-		int    mYOffSet;
-		FreeTypeGlyphArea* mArea;
-
-		// Glyph metrics
-		FreeTypeExtents mMetrics;
-	};
-
-	enum {
-		FREETYPE_GLYPH_AREA_EMPTY    = 0,
-		FREETYPE_GLYPH_AREA_PARTIAL  = 1,
-		FREETYPE_GLYPH_AREA_FULL     = 2,
-		FREETYPE_GLYPH_AREA_LOCKED   = 1 << 15
-	};
-
-	struct FreeTypeGlyphArea
-	{
-		short state;
-		unsigned short level;
-		int x;
-		int y;
-		int width;
-		int height;
-
-		FT_UInt index;
-
-		FreeTypeGlyphArea * children[4];
-	};
-
-	const unsigned int MAX_CACHED_IMAGES = 3;
-	const unsigned int DEFAULT_CACHE_ORDER = 7;
-	const unsigned int MAX_CACHE_LEVEL = 64;
 
 	class FreeTypeFont : public Font
 	{
@@ -96,50 +39,7 @@ namespace Sexy
 		virtual Font*			Duplicate();
 
 	private:
-		FreeTypeBaseFont	      * mBaseFont;
-		FT_Matrix			mMatrix;
-		float				mSize;
-
-		FT_Face				mFace;
-
-		int				mDescent;
-
-		typedef std::map<FT_UInt, FreeTypeGlyphEntry> GlyphMap;
-		typedef std::vector<FreeTypeGlyph>  GlyphVector;
-
-		// Glyph info cache
-		GlyphMap			mGlyphMap;
-
-		// Glyph Image cache
-		Image*				mImages[MAX_CACHED_IMAGES];
-		int				mImageSizeOrder[MAX_CACHED_IMAGES];
-		FreeTypeGlyphArea		mImageAreas[MAX_CACHED_IMAGES];
-
-		void				LockFace();
-		void				UnlockFace();
-
-		FreeTypeGlyphEntry*		LoadGlyph(FT_UInt index, bool render = false);
-		FreeTypeGlyphEntry*		LookupGlyph(FT_UInt index, bool render = false);
-		FreeTypeExtents*		LookupGlyphMetrics(FT_UInt index);
-
-		// Glyph cached area
-		FreeTypeGlyphArea*		FindGlyphAreaInArea(int width, int height, FT_UInt index,
-								    FreeTypeGlyphArea* area, bool remove = false);
-		FreeTypeGlyphArea*		FindGlyphArea(int width, int height, FT_UInt index, Image** image);
-		FreeTypeGlyphArea*		FindAnAreaToRemove(FreeTypeGlyphArea* area);
-
-		void				RemoveGlyphImage(FT_UInt index);
-
-		FreeTypeGlyphArea*		FreeTypeGlyphAreaCreate(int x, int y, int width, int height, int level);
-		void				FreeTypeGlyphAreaFree(FreeTypeGlyphArea* area);
-
-		void				ShrinkGlyphCache(void);
-
-		int				Utf8FromString(const std::string& string,
-							       std::string& utf8);
-
-		void				GlyphsFromString(const std::string& string, GlyphVector& glyphs,
-								 bool render = false);
+		FreeTypeScaledFont	      * mScaledFont;
 	};
 
 }
