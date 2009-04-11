4a171628b3838419601ae9409c540fb4c0f529dd
Luo Jinghua <sunmoon1997@gmail.com>
2009-04-11 15:24:16 +0800

    SexyAppFramework: add an environment variable to disable rectangle texture.

--- osframework/source/SexyAppFramework/GLInterface.cpp
+++ osframework/source/SexyAppFramework/GLInterface.cpp
@@ -152,10 +152,11 @@ void GLInterface::InitGL()
 		printf ("GL extensions: %s\n", mGLExtensions);
 
 	if (mGLExtensions) {
-		if (strstr (mGLExtensions, "GL_ARB_texture_non_power_of_two") ||
-		    strstr (mGLExtensions, "GL_EXT_texture_non_power_of_two") ||
-		    strstr (mGLExtensions, "GL_ARB_texture_rectangle") ||
-		    strstr (mGLExtensions, "GL_EXT_texture_rectangle"))
+		if ((strstr (mGLExtensions, "GL_ARB_texture_non_power_of_two") ||
+		     strstr (mGLExtensions, "GL_EXT_texture_non_power_of_two") ||
+		     strstr (mGLExtensions, "GL_ARB_texture_rectangle") ||
+		     strstr (mGLExtensions, "GL_EXT_texture_rectangle")) &&
+		    !getenv("SEXY_GL_NO_RECTANGLE_TEXTURE"))
 			mTextureNPOT = GL_TRUE;
 		if (mGLMajor > 2 || (mGLMajor == 1 && mGLMinor >= 2) ||
 		    strstr (mGLExtensions, "GL_IMG_texture_format_BGRA888") ||
