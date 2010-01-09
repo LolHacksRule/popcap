04a7c287d3e6d96acfbd8905f4d2b7701eaa59bc
Luo Jinghua <sunmoon1997@gmail.com>
2010-01-09 11:48:00 +0800

    uniconv: update the lastest version

--- osframework/source/SexyAppFramework/uniconv/uniconv.c
+++ osframework/source/SexyAppFramework/uniconv/uniconv.c
@@ -10,9 +10,15 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define UNICONV_MODE_IGNORE   (1 << 0)
+#define UNICONV_MODE_TRANSLIT (1 << 1)
+
 struct _uniconv {
     struct converter *from;
     struct converter *to;
+    char from_charset[64];
+    char to_charset[64];
+    int mode;
 };
 
 static char
@@ -23,9 +29,22 @@ uniconv_lower(char c)
     return c;
 }
 
+static char *
+uniconv_strrstr(const char *str, const char *token)
+{
+    int len = strlen(token);
+    const char *p = str + strlen(str);
+
+    while (str <= --p)
+        if (p[0] == token[0] && strncmp(p, token, len) == 0)
+            return (char *)p;
+    return NULL;
+}
+
 static const char*
 uniconv_canonical_charset(const char *charset,
-			  char *buf, size_t bufsz)
+			  char *buf, size_t bufsz,
+			  int *retmode)
 {
     size_t i;
     size_t len = strlen(charset);
@@ -39,6 +58,19 @@ uniconv_canonical_charset(const char *charset,
 	    buf[i] = uniconv_lower(charset[i]);
     }
 
+    if (retmode) {
+	char *p;
+
+	for (p = uniconv_strrstr(buf, "//"); p;
+	     p = uniconv_strrstr(buf, "//")) {
+	    if (!strcmp(p + 2, "ignore"))
+		*retmode |= UNICONV_MODE_IGNORE;
+	    else if (!strcmp(p + 2, "translit"))
+		*retmode |= UNICONV_MODE_TRANSLIT;
+	    *p = '\0';
+	}
+    }
+
     return get_canonical_charset(buf);
 }
 
@@ -46,18 +78,22 @@ uniconv_t*
 uniconv_open(const char *to, const char *from)
 {
     char frombuf[64];
-    char tobuf[64];
+    char tobuf[64 * 2];
+    int mode;
     uniconv_t *uc;
     struct converter *fc, *tc;
 
     if (!from || !to)
 	return NULL;
 
-    from = uniconv_canonical_charset(from, frombuf, sizeof(frombuf));
-    to = uniconv_canonical_charset(to, tobuf, sizeof(tobuf));
+    mode = 0;
+    from = uniconv_canonical_charset(from, frombuf, sizeof(frombuf), NULL);
+    to = uniconv_canonical_charset(to, tobuf, sizeof(tobuf), &mode);
     if (!from || !to)
 	return NULL;
 
+    if (strlen(from) > 63 || strlen(to) > 63)
+	return NULL;
     fc = converter_open(from);
     tc = converter_open(to);
     if (!fc || !tc)
@@ -68,6 +104,9 @@ uniconv_open(const char *to, const char *from)
 	goto close_conv;
     uc->from = fc;
     uc->to = tc;
+    uc->mode = mode;
+    strcpy(uc->from_charset, from);
+    strcpy(uc->to_charset, to);
     return uc;
  close_conv:
     converter_close(fc);
@@ -103,14 +142,14 @@ uniconv_conv(uniconv_t *uc,
 	return UNICONV_EBADF;
 
     /* reset converter */
-    if (!inbuf && !outbuf) {
+    if ((!inbuf || !(*inbuf)) && (!outbuf || !(*outbuf))) {
 	converter_reset(uc->from);
 	converter_reset(uc->to);
 	return UNICONV_SUCCESS;
     }
 
     /* converting/pushing input data */
-    if (inbuf) {
+    if (inbuf && *inbuf) {
 	uc_char_t ucs4;
 	uc_char_t *ucsbuf;
 
@@ -133,8 +172,11 @@ uniconv_conv(uniconv_t *uc,
 	    ucs4len = ucsbuf - &ucs4;
 	    ucsbuf = &ucs4;
 	    aoutbuf = *outbuf;
+
 	    ret = uc->to->encode(uc->to, (const ucs4_t **)&ucsbuf, ucs4len,
 				 outbuf, *outleft);
+	    if (ret == UNICONV_EILSEQ && uc->mode & UNICONV_MODE_IGNORE)
+		ret = UNICONV_SUCCESS;
 	    *outleft -= *outbuf - aoutbuf;
 	    if (ret < 0)
 		break;
@@ -143,6 +185,7 @@ uniconv_conv(uniconv_t *uc,
 	}
     } else {
 	/* converting pending data in buffer */
+	assert (outbuf && *outbuf);
 	saved_inbuf = NULL;
 	saved_outbuf = *outbuf;
 	ret = uc->to->encode(uc->to, NULL, 0, outbuf, *outleft);
--- osframework/source/SexyAppFramework/uniconv/utfconverter.c
+++ osframework/source/SexyAppFramework/uniconv/utfconverter.c
@@ -8,9 +8,25 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define FLAG_USE_BOM_ENDIAN  (1 << 0)
+#define FLAG_DONE_BOM_ENDIAN (1 << 1)
 struct utfconverter {
     struct converter base;
+
+    unsigned int flags;
+    int little_endian;
 };
+
+static int
+is_big_endian()
+{
+    static const union {
+	int iv;
+	char cv[4];
+    } u = { 0x12345678 };
+
+    return u.cv[0] == 0x12;
+}
 
 static int
 utf8_encode(struct converter *conv,
@@ -75,23 +91,39 @@ utf16_encode(struct converter *conv,
 	     char **outbuf,
 	     size_t outleft)
 {
+    struct utfconverter *uc = (struct utfconverter*)conv;
     size_t i;
-    uc_uint16_t **soutbuf = (uc_uint16_t **)outbuf;
 
     if (!inbuf)
 	return UNICONV_SUCCESS;
 
     for (i = 0; i < inleft; i++) {
 	int seqlen = ucs4toutf16(**inbuf, NULL);
+	uc_uint16_t utf16[2];
 	if (seqlen < 0)
 	    return UNICONV_EILSEQ;
-	if (seqlen * sizeof(uc_uint16_t) > outleft)
+	if (seqlen * 2 > outleft)
 	    return UNICONV_E2BIG;
 
-	ucs4toutf16(**inbuf, *soutbuf);
+	ucs4toutf16(**inbuf, utf16);
+	if (uc->little_endian) {
+	    (*outbuf)[0] = utf16[0] & 0xff;
+	    (*outbuf)[1] = utf16[0] >> 8;
+	    if (seqlen == 2) {
+		(*outbuf)[2] = utf16[1] & 0xff;
+		(*outbuf)[3] = utf16[1] >> 8;
+	    }
+	} else {
+	    (*outbuf)[0] = utf16[0] >> 8;
+	    (*outbuf)[1] = utf16[0] & 0xff;
+	    if (seqlen == 2) {
+		(*outbuf)[2] = utf16[1] >> 8;
+		(*outbuf)[3] = utf16[1] & 0xff;
+	    }
+	}
 	(*inbuf) += 1;
-	(*soutbuf) += seqlen;
-	outleft -= seqlen;
+	(*outbuf) += seqlen * 2;
+	outleft -= seqlen * 2;
     }
 
     return UNICONV_SUCCESS;
@@ -99,17 +131,39 @@ utf16_encode(struct converter *conv,
 
 static int
 utf16_decode(struct converter *conv,
-	     const char **inbuf,
+	     const char **sinbuf,
 	     size_t inleft,
 	     uc_char_t **outbuf,
 	     size_t outleft)
 {
-    const uc_uint16_t **sinbuf = (const uc_uint16_t**)inbuf;
+    struct utfconverter *uc = (struct utfconverter*)conv;
+    const uc_uint8_t **inbuf = (const uc_uint8_t **)sinbuf;
 
     while (inleft) {
 	uc_char_t unichar;
-	int seqlen = ucs4fromutf16(*sinbuf, &unichar,
-				   inleft / sizeof(uc_uint16_t));
+	uc_uint16_t utf16[2];
+	int seqlen;
+
+	if (inleft < 2)
+	    return UNICONV_EINVAL;
+
+	if (uc->little_endian)
+	    utf16[0] = (*inbuf)[1] << 8 | (*inbuf)[0];
+	else
+	    utf16[0] = (*inbuf)[0] << 8 | (*inbuf)[1];
+	if (utf16[0] >= 0xd800 && utf16[0] <= 0xbeff)
+	    seqlen = 2;
+	else
+	    seqlen = 1;
+	if (inleft < seqlen * 2)
+	    return UNICONV_EINVAL;
+	if (seqlen == 2) {
+	    if (uc->little_endian)
+		utf16[1] = (*inbuf)[3] << 8 | (*inbuf)[2];
+	    else
+		utf16[1] = (*inbuf)[2] << 8 | (*inbuf)[3];
+	}
+	seqlen = ucs4fromutf16(utf16, &unichar, seqlen);
 	if (seqlen == -2)
 	    return UNICONV_EINVAL;
 	else if (seqlen < 0)
@@ -117,12 +171,19 @@ utf16_decode(struct converter *conv,
 	if (!outleft)
 	    return UNICONV_E2BIG;
 
+	/* BOM */
+	if (unichar == 0xfffe && uc->flags & FLAG_USE_BOM_ENDIAN &&
+	    !(uc->flags & FLAG_DONE_BOM_ENDIAN)) {
+	    uc->flags &= ~FLAG_DONE_BOM_ENDIAN;
+	    uc->little_endian ^= 1;
+	    unichar = 0xfeff;
+	}
 	**outbuf = unichar;
 	(*outbuf) += 1;
 	outleft -= 1;
 
-	(*sinbuf) += seqlen;
-	inleft -= seqlen * sizeof(uc_uint16_t);
+	(*inbuf) += seqlen * 2;
+	inleft -= seqlen * 2;
     }
 
     return UNICONV_SUCCESS;
@@ -135,33 +196,79 @@ utf32_encode(struct converter *conv,
 	     char **outbuf,
 	     size_t outleft)
 {
+    struct utfconverter *uc = (struct utfconverter*)conv;
+
     if (!inbuf)
 	return UNICONV_SUCCESS;
 
-    if (inleft * sizeof(uc_char_t) > outleft)
-	return UNICONV_E2BIG;
+    while (inleft) {
+	if (outleft < 4)
+	    return UNICONV_E2BIG;
 
-    memcpy(*outbuf, *inbuf, inleft * sizeof(uc_char_t));
-    (*inbuf) += inleft;
-    (*outbuf) += inleft * sizeof(uc_char_t);
+	if (uc->little_endian) {
+	    (*outbuf)[0] = ((**inbuf) & 0x000000ff) >>  0;
+	    (*outbuf)[1] = ((**inbuf) & 0x0000ff00) >>  8;
+	    (*outbuf)[2] = ((**inbuf) & 0x00ff0000) >> 16;
+	    (*outbuf)[3] = ((**inbuf) & 0xff000000) >> 24;
+	} else {
+	    (*outbuf)[3] = ((**inbuf) & 0x000000ff) >>  0;
+	    (*outbuf)[2] = ((**inbuf) & 0x0000ff00) >>  8;
+	    (*outbuf)[1] = ((**inbuf) & 0x00ff0000) >> 16;
+	    (*outbuf)[0] = ((**inbuf) & 0xff000000) >> 24;
+	}
+	(*inbuf) += 1;
+	(*outbuf) += 4;
+	inleft -= 1;
+	outleft -= 4;
+    }
+
     return UNICONV_SUCCESS;
 }
 
 static int
 utf32_decode(struct converter *conv,
-	    const char **inbuf,
-	    size_t inleft,
-	    uc_char_t **outbuf,
-	    size_t outleft)
+	     const char **sinbuf,
+	     size_t inleft,
+	     uc_char_t **outbuf,
+	     size_t outleft)
 {
+    struct utfconverter *uc = (struct utfconverter*)conv;
+    const uc_uint8_t **inbuf = (const uc_uint8_t **)sinbuf;
+
     if (inleft & 3)
 	return UNICONV_EINVAL;
-    if (inleft > outleft)
-	return UNICONV_E2BIG;
 
-    memcpy(*outbuf, *inbuf, inleft);
-    (*inbuf) += inleft;
-    (*outbuf) += inleft / sizeof(uc_char_t);
+    while (inleft) {
+	if (!outleft)
+	    return UNICONV_E2BIG;
+
+	if (uc->little_endian)
+	    **outbuf =
+		((*inbuf)[0] <<  0) |
+		((*inbuf)[1] <<  8) |
+		((*inbuf)[2] << 16) |
+		((*inbuf)[3] << 24);
+	else
+	    **outbuf =
+		((*inbuf)[3] <<  0) |
+		((*inbuf)[2] <<  8) |
+		((*inbuf)[1] << 16) |
+		((*inbuf)[0] << 24);
+
+	/* BOM */
+	if (**outbuf == 0xfffe && uc->flags & FLAG_USE_BOM_ENDIAN &&
+	    !(uc->flags & FLAG_DONE_BOM_ENDIAN)) {
+	    uc->flags &= ~FLAG_DONE_BOM_ENDIAN;
+	    uc->little_endian ^= 1;
+	    **outbuf = 0xfeff;
+	}
+
+	(*inbuf) += 4;
+	(*outbuf) += 1;
+	inleft -= 4;
+	outleft -= 1;
+    }
+
     return UNICONV_SUCCESS;
 }
 
@@ -171,30 +278,66 @@ utfconverter_close(struct converter *conv)
     free(conv);
 }
 
+static void
+utfconverter_reset(struct converter *suc)
+{
+    struct utfconverter *uc = (struct utfconverter*)suc;
+
+    /* default to host endian, should be big endian? */
+    if (uc->flags & FLAG_USE_BOM_ENDIAN)
+	uc->little_endian = !is_big_endian();
+    uc->flags &= ~FLAG_DONE_BOM_ENDIAN;
+}
+
 struct converter *
 utfconverter_open(const char *charset)
 {
     struct utfconverter *conv;
 
-    if (strcmp(charset, "utf_8") && strcmp(charset, "utf_16") &&
-	strcmp(charset, "utf_32"))
+    if (strcmp(charset, "utf_8") &&
+	strcmp(charset, "utf_16") &&
+	strcmp(charset, "utf_16_le") &&
+	strcmp(charset, "utf_16_be") &&
+	strcmp(charset, "utf_32") &&
+	strcmp(charset, "utf_32_le") &&
+	strcmp(charset, "utf_32_be"))
 	return NULL;
 
     conv = malloc(sizeof(struct utfconverter));
     if (!conv)
 	return NULL;
+    conv->flags = 0;
+    conv->little_endian = !is_big_endian();
     if (!strcmp(charset, "utf_8")) {
 	conv->base.encode = utf8_encode;
 	conv->base.decode = utf8_decode;
     } else if (!strcmp(charset, "utf_16")) {
 	conv->base.encode = utf16_encode;
 	conv->base.decode = utf16_decode;
+	conv->flags |= FLAG_USE_BOM_ENDIAN;
+    } else if (!strcmp(charset, "utf_16_le")) {
+	conv->base.encode = utf16_encode;
+	conv->base.decode = utf16_decode;
+	conv->little_endian = 1;
+    } else if (!strcmp(charset, "utf_16_be")) {
+	conv->base.encode = utf16_encode;
+	conv->base.decode = utf16_decode;
+	conv->little_endian = 0;
     } else if (!strcmp(charset, "utf_32")) {
 	conv->base.encode = utf32_encode;
 	conv->base.decode = utf32_decode;
+	conv->flags |= FLAG_USE_BOM_ENDIAN;
+    } else if (!strcmp(charset, "utf_32_le")) {
+	conv->base.encode = utf32_encode;
+	conv->base.decode = utf32_decode;
+	conv->little_endian = 1;
+    } else if (!strcmp(charset, "utf_32_be")) {
+	conv->base.encode = utf32_encode;
+	conv->base.decode = utf32_decode;
+	conv->little_endian = 0;
     }
     conv->base.close = utfconverter_close;
-    conv->base.reset = NULL;
+    conv->base.reset = utfconverter_reset;
 
     return &conv->base;
 }
