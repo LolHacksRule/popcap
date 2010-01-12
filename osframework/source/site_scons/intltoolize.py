da18ad77e546368383f8ed7ed274c9a82730f9b5
Luo Jinghua <sunmoon1997@gmail.com>
2010-01-12 15:07:20 +0800

    intltoolize: add dtr to keyword list

--- osframework/source/site_scons/intltoolize.py
+++ osframework/source/site_scons/intltoolize.py
@@ -97,7 +97,8 @@ def intltoolize(env, srcdir, podirname, domain,
         if package is None:
             package = domain
         command = '$XGETTEXT --package-name=%s --package-version=%s ' \
-                  '-o $TARGET --keyword=tr --keyword=tr_noop -f $SOURCE' % \
+                  '-o $TARGET --keyword=tr --keyword=tr_noop --keyword=dtr:2 ' \
+                  '-f $SOURCE' % \
                   (package, package_version)
         potbuild = env.Command (os.path.join(buildpodir, potbuild_name), potfiles,
                                 command)
