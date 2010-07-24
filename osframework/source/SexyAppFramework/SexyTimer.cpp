65b738c51807ebf80839b75784ee3b5d5dab8aaf
Luo Jinghua <sunmoon1997@gmail.com>
2010-07-24 11:07:11 +0800

    SexyAppFramework: add absolute time getting on darwin

--- osframework/source/SexyAppFramework/SexyTimer.cpp
+++ osframework/source/SexyAppFramework/SexyTimer.cpp
@@ -5,6 +5,11 @@
 #include <sys/time.h>
 #endif
 
+#ifdef __APPLE__
+#include <mach/mach.h>
+#include <mach/mach_time.h>
+#endif
+
 DWORD Sexy::GetTickCount()
 {
 #ifdef WIN32
@@ -17,6 +22,17 @@ DWORD Sexy::GetTickCount()
 	ticks = now.tv_sec * 1000L +
 		now.tv_nsec / 1000000L;
 	return ticks;
+#elif defined(__APPLE__)
+	static mach_timebase_info_data_t timebaseInfo;
+	uint64_t cur, nano;
+
+	cur = mach_absolute_time();
+
+	if (timebaseInfo.denom == 0)
+	  (void) mach_timebase_info(&timebaseInfo);
+
+	nano = cur * timebaseInfo.numer / timebaseInfo.denom;
+	return nano / 1000000L;
 #else
 	struct timeval now;
 	DWORD ticks;
