5b03005464409162f4deee901cd1aa63f718860d
Luo Jinghua <sunmoon1997@gmail.com>
2010-08-15 05:41:51 +0800

    demos: update the main.mm

--- osframework/source/demos/Demo5/main.mm
+++ osframework/source/demos/Demo5/main.mm
@@ -5,10 +5,17 @@
 using namespace Sexy;
 
 int main(int argc, char **argv)
-{	
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+	[[NSFileManager defaultManager] changeCurrentDirectoryPath: [[NSBundle mainBundle] resourcePath]];
+
 	GameApp* anApp = new GameApp();
+
 	int retval = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+
 	delete anApp;
-	
+
+	[pool release];
 	return retval;
 }
