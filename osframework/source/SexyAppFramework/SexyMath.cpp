989b0c215befcc97e101695cce02b66a5cf94d1c
Luo Jinghua <sunmoon1997@gmail.com>
2010-11-18 18:13:56 +0800

    SexyAppFramework: added fast version of tri functions

---
+++ osframework/source/SexyAppFramework/SexyMath.cpp
@@ -0,0 +1,85 @@
+#include "SexyMath.h"
+
+#include <limits.h>
+#include <math.h>
+
+#ifndef INFINITY
+#define INFINITY HUG_VAL
+#endif
+
+#include "SexyMath.inl"
+
+namespace Sexy {
+
+#define MAX_STEP 4096
+
+float fastsinf(float x)
+{
+	int index = int((x * MAX_STEP) / (pi * 2)) + MAX_STEP;
+	return sin_tab[index % MAX_STEP];
+}
+
+float fastcosf(float x)
+{
+	int index = int((x * MAX_STEP) / (pi * 2)) + MAX_STEP;
+	return cos_tab[index % MAX_STEP];
+}
+
+float fastasinf(float x)
+{
+	int index = int((x + 1.0f) * (MAX_STEP / 2)) + MAX_STEP;
+	return asin_tab[index % MAX_STEP];
+}
+
+float fastacosf(float x)
+{
+	int index = int((x + 1.0f) * (MAX_STEP / 2)) + MAX_STEP;
+	return acos_tab[index % MAX_STEP];
+}
+
+float fasttanf(float x)
+{
+	int index = int(floor(x * MAX_STEP / pi + 0.5)) + MAX_STEP;
+	return tan_tab[index % MAX_STEP];
+}
+
+float fastatanf(float x)
+{
+	int a, b, c;
+	float d;
+
+	if (x > 0.0)
+	{
+		a = 0;
+		b = (MAX_STEP / 2) - 1;
+	}
+	else
+	{
+		a = MAX_STEP / 2;
+		b = MAX_STEP;
+	}
+
+	do
+	{
+		c = (a + b) >> 1;
+		d = x - tan_tab[c];
+
+		if (d > 0)
+			a = c + 1;
+		else if (d < 0)
+			b = c - 1;
+
+	} while ((a <= b) && (d));
+
+	if (x > 0)
+		return (c * pi) / MAX_STEP;
+
+	return (c - MAX_STEP) * pi / MAX_STEP;
+}
+
+float fastatan2f(float y, float x)
+{
+	return fasttanf(y / x);
+}
+
+}
---
+++ osframework/source/SexyAppFramework/SexyMath.h
@@ -0,0 +1,15 @@
+#ifndef __SEXY_MATH_H__
+#define __SEXY_MATH_H__
+
+namespace Sexy {
+
+  float fastsinf(float x);
+  float fastcosf(float x);
+  float fastasinf(float x);
+  float fastacosf(float x);
+  float fasttanf(float x);
+  float fastatanf(float x);
+  float fastatan2f(float y, float x);
+}
+
+#endif
