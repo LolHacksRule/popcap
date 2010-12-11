c339efdba213a2f6d032bc45c211eacb326fce2d
Luo Jinghua <sunmoon1997@gmail.com>
2010-12-11 22:21:20 +0800

    hungarr2: support android

---
+++ osframework/source/demos/Hun-garr2/android/src/org/jinghua/hungarr2/GameLauncher.java
@@ -0,0 +1,11 @@
+package org.jinghua.hungarr2;
+
+import org.jinghua.GameView;
+import org.jinghua.GameActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameLauncher extends GameActivity
+{
+}
