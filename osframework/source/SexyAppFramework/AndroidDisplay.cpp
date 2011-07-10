49e6d0f5448c61cb1b7be32cd592da95814c315f
Luo Jinghua <sunmoon1997@gmail.com>
2011-07-10 13:02:56 +0800

    SexyAppFramework: fixed compilation borked by previous changes

--- osframework/source/SexyAppFramework/AndroidDisplay.cpp
+++ osframework/source/SexyAppFramework/AndroidDisplay.cpp
@@ -20,6 +20,7 @@
 
 #define  LOG_TAG    "AndroidDisplay"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 #define printf LOGD
 #endif
