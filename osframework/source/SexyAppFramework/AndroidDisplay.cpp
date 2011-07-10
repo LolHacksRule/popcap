56c703a10cc909fb1eee48ee7369c47aac2e357c
Luo Jinghua <sunmoon1997@gmail.com>
2011-07-10 12:34:18 +0800

    SexyAppFramework: change log level to debug

--- osframework/source/SexyAppFramework/AndroidDisplay.cpp
+++ osframework/source/SexyAppFramework/AndroidDisplay.cpp
@@ -21,7 +21,7 @@
 #define  LOG_TAG    "AndroidDisplay"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-#define printf LOGI
+#define printf LOGD
 #endif
 
 using namespace Sexy;
