f93f130b50e3edeaaaef9d7535d14b3313dbfc2c
Luo Jinghua <sunmoon1997@gmail.com>
2011-11-13 14:16:28 +0800

    SexyAppFramework: Reword the log message

--- osframework/source/SexyAppFramework/SexyLogListener.cpp
+++ osframework/source/SexyAppFramework/SexyLogListener.cpp
@@ -1,6 +1,7 @@
 #include "SexyLogListener.h"
 #include "SexyLogManager.h"
 #include "Common.h"
+#include "SexyTimer.h"
 
 #if defined(WIN32) || defined(_WIN32)
 #include <windows.h>
@@ -14,19 +15,27 @@ using namespace Sexy;
 
 std::string LogListener::formatMessage(LogLevel lvl, const std::string& tag, const std::string& s)
 {
-	LogFormat format = mFormat;
+        LogFormat format = mFormat;
 
-	std::string prefix;
+        std::string prefix;
 
-	if (format == LOG_FORMAT_DEFAULT)
-		format = LogManager::getInstance().getDefaultFormat();
+        if (format == LOG_FORMAT_DEFAULT)
+                format = LogManager::getInstance().getDefaultFormat();
 
-	if (format & LOG_FORMAT_PID)
-		prefix += StrFormat("%u", (unsigned)getpid());
-	if (format & LOG_FORMAT_TAG)
-		prefix += prefix.empty() ? tag : std::string("/") + tag;
-	if (format & LOG_FORMAT_TIMESTAMP)
-		prefix += StrFormat(" %u", (unsigned)GetTickCount());
-	return prefix + " " + s;
+        if (format & LOG_FORMAT_TAG)
+                prefix += prefix.empty() ? tag : std::string("/") + tag;
+        if (format & LOG_FORMAT_PID)
+                prefix += StrFormat("/%u", (unsigned)getpid());
+
+        std::string lvlName(logLevelName(lvl));
+        inlineUpper(lvlName);
+        prefix += " " + lvlName;
+
+        if (format & LOG_FORMAT_TIMESTAMP)
+        {
+                unsigned int tick = GetTickCount();
+                prefix += StrFormat(" %u.%03u", tick / 1000, tick % 1000);
+        }
+        return prefix + ": " + s;
 }
 
