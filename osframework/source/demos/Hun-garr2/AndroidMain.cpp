c3cc91d9818606897a37a7a742cdde5960f2442d
Luo Jinghua <sunmoon1997@gmail.com>
2011-11-13 14:12:32 +0800

    Hungarr2: Removed all android specific code in AndroidMain.cpp

--- osframework/source/demos/Hun-garr2/AndroidMain.cpp
+++ osframework/source/demos/Hun-garr2/AndroidMain.cpp
@@ -1,14 +1,5 @@
 #include "GameApp.h"
 
-#include <unistd.h>
-
-#include <android/log.h>
-#include <jni.h>
-
-#define  LOG_TAG    "Hungarr2"
-#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-
 using namespace Sexy;
 
 static GameApp *app;
@@ -21,19 +12,10 @@ GameInit()
     if (app)
 	return -1;
 
-    char path[2048];
-
-    getcwd(path, sizeof(path));
-
-    LOGI("current working directory: %s\n", path);
-    LOGI("starting Hungarr2...\n");
     app = new GameApp();
     app->mAllowSleep = false;
-    LOGI("Initalizing Hungarr2...\n");
     app->Init();
-    LOGI("Hungarr2 intialized.\n");
     app->Startup();
-    LOGI("Hungarr2 set to run state now.\n");
 
     return 0;
 }
@@ -58,18 +40,7 @@ GameRender(void)
     if (!app)
 	return 0;
 
-    int count;
-    while (true)
-    {
-	count = app->mDrawCount;
-	app->UpdateApp();
-	if (app->mShutdown)
-	    return 0;
-	if (app->mDrawCount != count)
-	    break;
-    }
-
-    return 1;
+    return app->DrawOneFrame() ? 1 : 0;
 }
 
 }
